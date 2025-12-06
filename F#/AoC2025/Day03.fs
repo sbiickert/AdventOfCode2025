92dce935a667e896e4bae853df20fbdaa0bdb8bc
Simon Biickert <simon@biickert.ca>
2025-12-05 18:23:28 -0700

    Day 3 complete in F#

--- F#/AoC2025/Day03.fs
+++ F#/AoC2025/Day03.fs
@@ -17,8 +17,39 @@ let solvePartOne input =
     |> List.map (fun bank -> maxJoltageForBankPartOne bank)
     |> List.sum
 
+// https://www.reddit.com/r/adventofcode/comments/1pdk5zj/2025_day_3_part_2_python_am_i_misunderstanding/
+
+let indexToDrop (batteries:list<int>): int =
+    let mutable result = -1
+    let len = List.length batteries
+
+    for i = 0 to len - 1 do
+        if result >= 0 then
+            ()
+        elif i + 1 = len then
+            result <- i
+        elif batteries[i] < batteries[i+1] then
+            result <- i
+            
+    result
+
 let maxJoltageForBankPartTwo (bank:string): int64 =
-    1
+    let mutable batteries = 
+        bank
+        |> Seq.map (fun c -> int (string c)) 
+        |> Seq.toList
+
+    while List.length batteries > 12 do
+        let dropI = indexToDrop batteries
+        batteries <- batteries
+        |> List.mapi (fun index n -> (index <> dropI, n))
+        |> List.filter fst
+        |> List.map snd
+        
+    batteries
+    |> List.map (fun i -> string i)
+    |> String.concat ""
+    |> int64
 
 let solvePartTwo input =
     input
