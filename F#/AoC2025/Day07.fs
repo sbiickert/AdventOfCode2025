ab91f4c7ef3e4d954956fe99b66dc792bf1ff48b
Simon Biickert <simon@biickert.ca>
2025-12-18 12:13:57 -0700

    Day 7 complete in F#

--- F#/AoC2025/Day07.fs
+++ F#/AoC2025/Day07.fs
@@ -28,7 +28,28 @@ let solvePartOne manifold =
     splitCount
 
 let solvePartTwo manifold =
-    2
+    let startCoord = (Grid.coords (Some "S") manifold).Head
+    let width = int (Extent.widthOf manifold.extent.Value) + 2 // Grid loading code trimmed the "empty" edges
+    let height = int (Extent.heightOf manifold.extent.Value)
+    let mutable manifoldRow = Array.create width 0L
+    manifoldRow[int startCoord.x] <- 1
+
+    seq { 0 .. height }
+    |> Seq.iter (fun y -> 
+        let resultRow = Array.create width 0L
+        manifoldRow
+        |> Array.iteri (fun x count ->
+            if count > 0 then
+                let coord = mkCoord (int64 x) (int64 (y + 2))
+                if Grid.getString coord manifold = "^" then
+                    resultRow[x-1] <- resultRow[x-1] + count
+                    resultRow[x+1] <- resultRow[x+1] + count
+                else
+                    resultRow[x] <- resultRow[x] + count
+        )
+        manifoldRow <- resultRow
+    )
+    Array.sum manifoldRow
 
 let solveDay07 isTest: Unit =
     let day = 07
