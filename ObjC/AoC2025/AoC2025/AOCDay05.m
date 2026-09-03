b8aefed8c82124cddc7dbf6281350aa5786b9bcf
Simon Biickert <simon@biickert.ca>
2026-09-03 14:43:58 -0600

    Day 5 part 2 complete in ObjC

--- ObjC/AoC2025/AoC2025/AOCDay05.m
+++ ObjC/AoC2025/AoC2025/AOCDay05.m
@@ -5,6 +5,7 @@
 
 #import <Foundation/Foundation.h>
 #import "AOCDay.h"
+#import "AOCStrings.h"
 
 @implementation AOCDay05
 
@@ -23,7 +24,7 @@
 	NSArray<NSNumber *> *ids = [self parseIDs:idInput];
 
 	result.part1 = [self solvePartOne: ranges ids:ids];
-	result.part2 = [self solvePartTwo: idInput];
+	result.part2 = [self solvePartTwo: ranges];
 	
 	return result;
 }
@@ -44,9 +45,42 @@
 	return [NSString stringWithFormat: @"Fresh ingredient count is %ld", (long)freshCount];
 }
 
-- (NSString *)solvePartTwo:(NSArray<NSString *> *)input {
+- (NSString *)solvePartTwo:(NSArray<NSValue *> *)ranges {
+	NSArray<NSValue *> *newRanges = [self coalesceRanges:ranges];
 	
-	return [NSString stringWithFormat: @"World %ld", (long)42];
+	NSInteger freshCount = 0;
+	
+	for (NSValue *val in newRanges){
+		NSRange range = val.rangeValue;
+		freshCount += range.length + 1;
+	}
+	
+	return [NSString stringWithFormat: @"Fresh ID count is %ld", (long)freshCount];
+}
+
+- (NSArray<NSValue *> *)coalesceRanges:(NSArray<NSValue *> *)ranges {
+	NSMutableArray<NSValue *> *coalesced = ranges.mutableCopy; // ranges are sorted by location (i.e. start id)
+	
+	BOOL arrayIsChanged = YES;
+	
+	while (arrayIsChanged) {
+		arrayIsChanged = NO;
+		for (NSInteger i = 0; i < coalesced.count-1; i++) {
+			NSRange r1 = [coalesced objectAtIndex:i].rangeValue;
+			NSRange r2 = [coalesced objectAtIndex:i+1].rangeValue;
+			
+			if (NSMaxRange(r1) >= r2.location) {
+				NSUInteger upper = MAX(NSMaxRange(r1), NSMaxRange(r2));
+				NSRange unionRange = NSMakeRange(r1.location, upper - r1.location);
+				coalesced[i] = [NSValue valueWithRange:unionRange];
+				[coalesced removeObjectAtIndex:i+1];
+				arrayIsChanged = YES;
+				break;
+			}
+		}
+	}
+	
+	return coalesced;
 }
 
 - (NSArray<NSValue *>*)parseRanges:(NSArray<NSString *> *)input {
@@ -59,6 +93,12 @@
 		NSRange range = NSMakeRange(start, end - start);
 		[result addObject:[NSValue valueWithRange:range]];
 	}
+	
+	[result sortUsingComparator:^NSComparisonResult(NSValue *v1, NSValue *v2) {
+		NSNumber *n1 = [NSNumber numberWithInteger:v1.rangeValue.location];
+		NSNumber *n2 = [NSNumber numberWithInteger:v2.rangeValue.location];
+		return [n1 compare: n2];
+	}];
 	
 	return result;
 }
